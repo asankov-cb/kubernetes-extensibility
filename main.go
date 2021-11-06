c778d88644cfc586c193987125820c286ed00465
Anton Sankov <asankov@vmware.com>
2021-11-06 18:34:42 +0200

    Generate boilerplate for `Speaker` CRD
    - run `kubebuilder create api --group istacon --version v1 --kind Speaker`
    - add fields
    - run `make manifests` to generate CRD YAML

--- main.go
+++ main.go
@@ -85,6 +85,13 @@ func main() {
 		setupLog.Error(err, "unable to create controller", "controller", "ConferenceTalk")
 		os.Exit(1)
 	}
+	if err = (&controllers.SpeakerReconciler{
+		Client: mgr.GetClient(),
+		Scheme: mgr.GetScheme(),
+	}).SetupWithManager(mgr); err != nil {
+		setupLog.Error(err, "unable to create controller", "controller", "Speaker")
+		os.Exit(1)
+	}
 	//+kubebuilder:scaffold:builder
 
 	if err := mgr.AddHealthzCheck("healthz", healthz.Ping); err != nil {
